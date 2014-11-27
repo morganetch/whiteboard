4c97454a256d748a08d10dfa1e66f6e5a0006b26
Gilles Boom <gillesbm@gmail.com>
2014-11-27 23:27:21 +0100

    front-end whiteboard done Closes #17

--- view/boards/view.php
+++ view/boards/view.php
@@ -1,3 +1,42 @@
 <section id="content" class="view">
+	<header>
+		<div class="settings">
+			<a href="close"><img src="img/close.png" alt="close"></a>
+			<a href="settings"><img src="img/settings.png" alt="settings"></a>
+			<a href="save"><img src="img/save.png" alt="save"></a>
+		</div>
+		<div class="name">Whiteboard name</div>
+		<div class="account">Logged in as GillesBm</div>
+	</header>
+	<section class="buttons">
+		<a href="picture"><img src="img/picture.png" alt="picture"></a>
+		<a href="video"><img src="img/video.png" alt="video"></a>
+		<a href="text"><img src="img/balloon.png" alt="text"></a>
+	</section>
+	<section class="holder">
+		<article class="item picture">
+			<h1>Cute kitten</h1>
+			<img src="uploads/cutecat.jpg" alt="cute cat">
+			<p class="desc">Picture taken on 23/08/2013 when I was on vacation in Greece</p>
+			<a href="edit">Edit</a>
+			<a href="delete">Delete</a>
+		</article>
+
+		<article class="item video">
+			<h1>Sky Scream</h1>
+			<video src="uploads/skyscream.mp4" controls></video>
+			<p class="desc">Awesome onride video of Sky Scream attraction in Plopsa Holiday Park</p>
+			<a href="edit">Edit</a>
+			<a href="delete">Delete</a>
+		</article>
+
+		<article class="item text">
+			<h1>Lorem Ipsum</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque augue neque, ultricies vitae condimentum eget, consequat id enim. Suspendisse vulputate bibendum neque in aliquet. Aliquam ac elit tellus. Fusce et porttitor metus. Quisque facilisis nulla at lectus efficitur gravida. Phasellus nec lectus ligula. In quis turpis eget lacus finibus fermentum ut ut magna. Curabitur sed molestie tortor, quis posuere diam. Etiam pretium ex eu ullamcorper scelerisque. Aenean lobortis purus tincidunt tellus ullamcorper facilisis. Donec vestibulum, turpis eu placerat volutpat, metus felis aliquet urna, vel blandit eros diam vel tortor. Nulla facilisi. Duis ac porttitor purus. Donec ultrices lobortis mattis. Nulla eleifend, orci id fermentum porta, erat leo sagittis mi, vitae pharetra turpis diam sed urna.</p>
+			<p class="desc">This is some random text I used to fill in the paragraph</p>
+			<a href="edit">Edit</a>
+			<a href="delete">Delete</a>
+		</article>
+	</section>
     
 </section>
