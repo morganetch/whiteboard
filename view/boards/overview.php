a9f9dcb65e6d9e5171d2e8ee69d063b0b27898b2
Morgane Tchekouteff <morgane.tchekouteff@student.howest.be>
2014-12-02 11:01:40 +0100

    design the list of created whiteboards in your overviewpage

--- view/boards/overview.php
+++ view/boards/overview.php
@@ -9,20 +9,20 @@
     		<a href="index.php?page=add">Nieuwe whiteboard</a>
     	</div>
     	<div class="boards">
-			<div class="eigen-boards">
-		    	<?php foreach($ownBoards as $ownBoard) { ?>
-						<li>
-							<a href="index.php?page=view&id=<?php echo $ownBoard['id']; ?>"><?php echo $ownBoard['name']; ?></a>
-						</li>
-				<?php } ?>
-			</div>
-			<div class="invited-boards">
-		    	<?php foreach($invitedBoards as $invitedBoard) { ?>
-						<li>
-							<a href="index.php?page=view&id=<?php echo $invitedBoard['id']; ?>"><?php echo $invitedBoard['name']; ?></a>
-						</li>
-				<?php } ?>
-			</div>
+			<ol class="eigen-boards">
+		    <?php foreach($ownBoards as $ownBoard) { ?>
+				<li>
+					<a href="index.php?page=view&id=<?php echo $ownBoard['id']; ?>"><?php echo $ownBoard['name']; ?></a>
+				</li>
+			<?php } ?>
+			</ol>
+			<ol class="invited-boards">
+		    <?php foreach($invitedBoards as $invitedBoard) { ?>
+				<li>
+					<a href="index.php?page=view&id=<?php echo $invitedBoard['id']; ?>"><?php echo $invitedBoard['name']; ?></a>
+				</li>
+			<?php } ?>
+			</ol>
 		</div>
     </article>
 </section>
