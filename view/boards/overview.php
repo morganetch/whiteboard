c7e0573bb84259a80f3e30d339b03347eb764a31
Morgane Tchekouteff <morgane.tchekouteff@student.howest.be>
2014-12-02 10:42:56 +0100

    show the list of created whiteboards in your overviewpage

--- view/boards/overview.php
+++ view/boards/overview.php
@@ -8,6 +8,22 @@
     	<div class="knop">
     		<a href="index.php?page=add">Nieuwe whiteboard</a>
     	</div>
+    	<div class="boards">
+			<div class="eigen-boards">
+		    	<?php foreach($ownBoards as $ownBoard) { ?>
+						<li>
+							<a href="index.php?page=view&id=<?php echo $ownBoard['id']; ?>"><?php echo $ownBoard['name']; ?></a>
+						</li>
+				<?php } ?>
+			</div>
+			<div class="invited-boards">
+		    	<?php foreach($invitedBoards as $invitedBoard) { ?>
+						<li>
+							<a href="index.php?page=view&id=<?php echo $invitedBoard['id']; ?>"><?php echo $invitedBoard['name']; ?></a>
+						</li>
+				<?php } ?>
+			</div>
+		</div>
     </article>
 </section>
 
